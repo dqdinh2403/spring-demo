937d84b1704a93b42cdaac77473e0465bffb2208
dqdinh <dinh.dao@axonactive.com>
2020-12-23 17:51:08 +0700

    23/12: bean validation + view controller

--- src/main/java/com/example/springdemo/controller/DesignController.java
+++ src/main/java/com/example/springdemo/controller/DesignController.java
@@ -5,12 +5,12 @@ import com.example.springdemo.domain.Ingredient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -32,7 +32,11 @@ public class DesignController {
     }
 
     @PostMapping
-    public String processDesign(Design design){
+    public String processDesign(@Valid Design design, Errors errors){
+        if(errors.hasErrors()){
+            return "design";
+        }
+
         // save the design
         log.info("Processing design: " + design);
 
--- src/main/java/com/example/springdemo/domain/Order.java
+++ src/main/java/com/example/springdemo/domain/Order.java
@@ -2,11 +2,15 @@ package com.example.springdemo.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class Order {
 
-    public String name;
+    @NotBlank(message = "Name is required")
+    private String name;
 
+    @NotBlank(message = "Zip is required")
     private String zip;
 
 }

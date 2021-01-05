fd47125aaa4ac985999bb3279e6a267a3b3b785d
dqdinh <dinh.dao@axonactive.com>
2021-01-05 18:56:52 +0700

    05/01: spring security with in-memory store user

--- src/main/java/com/example/springdemo/SecurityConfig.java
+++ src/main/java/com/example/springdemo/SecurityConfig.java
@@ -1,10 +1,37 @@
 package com.example.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // JDBC-based user store
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // In-memory user store
+//        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser("qoobee")
+//                .password(encoder.encode("qoobee"))
+//                .authorities("ROLE_USER");
+
+        // JDBC-based user store
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
+    }
+
 }

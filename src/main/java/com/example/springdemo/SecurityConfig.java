bfa6cffe4837f1d4895bd07e5f5c77399628d310
dqdinh <dinh.dao@axonactive.com>
2021-01-07 17:38:43 +0700

    07/01: spring security with LDAP-backer

--- src/main/java/com/example/springdemo/SecurityConfig.java
+++ src/main/java/com/example/springdemo/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
@@ -16,8 +17,8 @@ import javax.sql.DataSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // JDBC-based user store
-    @Autowired
-    DataSource dataSource;
+//    @Autowired
+//    DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -30,11 +31,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .authorities("ROLE_USER");
 
         // JDBC-based user store
-        auth.jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery("select username, password, enabled from Users where username = ?")
-                .authoritiesByUsernameQuery("select username, authority from UserAuthorities where username = ?")
-                .passwordEncoder(new StandardPasswordEncoder("secretKey"));
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .usersByUsernameQuery("select username, password, enabled from Users where username = ?")
+//                .authoritiesByUsernameQuery("select username, authority from UserAuthorities where username = ?")
+//                .passwordEncoder(new StandardPasswordEncoder("secretKey"));
+
+        // LDAP-backed user store
+//        auth.ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("{member={0}}")
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("passcode");
+
+
 
     }
 

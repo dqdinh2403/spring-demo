4a034b4cffb3c79b789dd6e8e10dc1ce93521ad5
dqdinh <dinh.dao@axonactive.com>
2021-01-19 18:17:17 +0700

    19/01: securing web request + custom login page

--- src/main/java/com/example/springdemo/SecurityConfig.java
+++ src/main/java/com/example/springdemo/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -32,7 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("qoobee")
                 .password(encoder.encode("qoobee"))
-                .authorities("ROLE_USER");
+                .authorities("USER");
 
         // JDBC-based user store
 //        auth.jdbcAuthentication()
@@ -59,5 +60,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    public PasswordEncoder encoder(){
 //        return new StandardPasswordEncoder("secretKey");
 //    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                    .hasRole("USER")
+                .antMatchers("/", "/**")
+                    .permitAll()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/design");
+    }
+
 
 }
